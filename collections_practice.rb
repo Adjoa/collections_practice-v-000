1a09e8d964b9bf6306cb502ce09afacca350488d
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 18:11:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -63,6 +63,6 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    index !=1 ?  array[index] << "s" : array[index]
+    index !=1 ?  element << "s" : element
   end
 end
