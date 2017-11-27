7b1cb6ff8b61a9ef5543333a7fdc357ff4007864
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 18:04:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|    
+  array.each_with_index.collect do |element, index|
       array[index] << "s" unless index = 1
   end
 end
