7830c76f652cbd3fa07e6e6757a218870996e462
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 18:24:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,24 +3,24 @@
 #     x <=> y
 #   end
 # end
-# 
+#
 # def sort_array_desc(array)
 #   array.sort do |x, y|
 #     y <=> x
 #   end
 # end
-# 
+#
 # def sort_array_char_count(strings)
 #   strings.sort do |x, y|
 #     x.length <=> y.length
 #   end
 # end
-# 
+#
 # def swap_elements(array)
 #   temp = array[1]
 #   array[1] = array[2]
 #   array[2] = temp
-# 
+#
 #   array
 # end
 
