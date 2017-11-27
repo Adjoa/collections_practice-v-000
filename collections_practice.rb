e043a19dcab19ed28a803a1c5d016a8d4f058338
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 18:24:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,13 +24,13 @@ def swap_elements(array)
   array
 end
 
-def swap_elements_from_to(array, from, to)
-  temp = array[from]
-  array[from] = array[to]
-  array[to] = temp
-
-  array
-end
+# def swap_elements_from_to(array, from, to)
+#   temp = array[from]
+#   array[from] = array[to]
+#   array[to] = temp
+# 
+#   array
+# end
 
 def reverse_array(array)
   reversed_array = []
