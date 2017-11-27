57ef6653579408c21ae07bd3b80d7e3bcfedb288
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 17:26:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,6 +20,15 @@ def swap_elements(array)
   temp = array[1]
   array[1] = array[2]
   array[2] = temp
-  
+
   array
+end
+
+def reverse_array(array)
+  i = array.length
+  reversed_array = []
+  while i > 0
+    reversed_array << array[i]
+    i -= 1
+  end
 end
