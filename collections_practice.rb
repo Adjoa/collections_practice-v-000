5feb2168fee1d86ed62c59f1297d15709a9af857
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 17:13:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort do |x, y|
+    x <=> y
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |x, y|
+    y <=> x
+  end
+end
+
+def sort_array_char_count(string_array)
+  
+end
