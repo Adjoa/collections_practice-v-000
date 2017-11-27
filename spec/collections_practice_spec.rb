f4b1acd23f55c233eef9f805cb3cbb032617b68c
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-27 18:24:24 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -31,12 +31,12 @@ describe 'collections practice' do
   end
 
   # Question 4 Advanced
-  describe '#swap_elements_from_to' do
-    it 'swap elements in an array based on indices given' do
-      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
-    end
-  end
-  
+  # describe '#swap_elements_from_to' do
+  #   it 'swap elements in an array based on indices given' do
+  #     expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+  #   end
+  # end
+
   end
   # Question 5
   describe '#reverse_array' do
